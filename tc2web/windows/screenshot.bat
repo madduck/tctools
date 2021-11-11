8bfccbcc8b6e3d707705e4ec35cfcb31a14b5d3f
martin f. krafft <madduck@madduck.net>
2021-11-12 10:57:09 +1300

    Alternative to qprocess to check for TC running

--- tc2web/windows/screenshot.bat
+++ tc2web/windows/screenshot.bat
@@ -7,9 +7,10 @@
 ::
 @echo off
 
-qprocess TournControl.exe >NUL 2>&1
+set TCexe=TournControl.exe
+tasklist /NH /FI "ImageName eq %TCexe%" | findstr /i "%TCexe%" >NUL 2>&1
 if %ERRORLEVEL% equ 0 (
-  "\Program Files (x86)\MiniCap\MiniCap.exe" -save %~f1 -captureappbyname "TournControl.exe" -noerr -closeapp -exit -nofocus
+  "\Program Files (x86)\MiniCap\MiniCap.exe" -save %~f1 -captureappbyname "%TCexe%" -noerr -closeapp -exit -nofocus
 ) else (
   echo TournamentControl is not running. >&2
 )
