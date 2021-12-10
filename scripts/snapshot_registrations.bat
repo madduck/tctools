e4592e5512eff13ed56d065fc33ac84004e0566c
martin f. krafft <madduck@madduck.net>
2021-12-10 14:47:00 +1300

    Batch script for snapshotting registrations on Windows

---
+++ scripts/snapshot_registrations.bat
@@ -0,0 +1,19 @@
+:: snapshot_registrations.bat — snapshot registrations
+::
+:: Copyright © 2021 martin f. krafft <tctools@pobox.madduck.net>
+:: Released under the terms of the MIT Licence
+
+@echo off
+setlocal
+
+cd /d %~dp0"
+
+set "SNAPSHOTDIR=..\snapshots"
+
+if [%~2]==[] goto doit
+
+set "OUTOPT=--output %2"
+
+:doit
+python manage_isquash_tournament.py --headless --seed --extract-registrations %SNAPSHOTDIR%\TIMESTAMP-registrations.xls
+python split_off_waitinglist.py --cutoff %1 %OUTOPT% --delete-nodiff %SNAPSHOTDIR%\*-registrations.xls
